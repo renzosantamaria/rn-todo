9bc9a51bfb49e7c5d4fc78bd44e197bfac258a3d
renzosantamaria <renzo.santamaria@outlook.com>
2022-05-14 16:50:34 +0200

    add wrapping Screen component to Register view

--- src/screens/Register.tsx
+++ src/screens/Register.tsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { useDispatch } from "react-redux";
 import { StyleSheet, View, Text } from "react-native";
 import Button from "../components/styled-components/Button";
+import Screen from "../components/Screen/Screen";
 import Input from "../components/Input";
 import * as Colors from "../constants/colors";
 import * as API from "../API";
@@ -25,44 +26,53 @@ const RegisterForm: React.FC<IProps> = (props) => {
   };
 
   return (
-    <View style={styles.registerForm}>
-      <Text style={styles.heading}>Register </Text>
-      <Input
-        style={"inputLight"}
-        placeholder={"name"}
-        value={name}
-        onChangeText={(text) => setName(text)}
-      />
-      <Input
-        style={"inputLight"}
-        placeholder={"LastName"}
-        value={lastname}
-        onChangeText={(text) => setLastname(text)}
-      />
-      <Input
-        style={"inputLight"}
-        placeholder={"Email"}
-        value={email}
-        onChangeText={(text) => setEmail(text)}
-      />
-      <Input
-        style={"inputLight"}
-        placeholder={"Password"}
-        value={password}
-        onChangeText={(text) => setPassword(text)}
-      />
+    <Screen
+      bgcolor="black"
+      header={{ hide: false, color: "#fff" }}
+      transparentBackground={true}
+      // showLoadingIndicator={props.isLoginGoogleLoading}
+      // loadingText="Logging in..."
+      ignorepadding={true}
+    >
+      <View style={styles.registerForm}>
+        <Text style={styles.heading}>Register </Text>
+        <Input
+          style={"inputLight"}
+          placeholder={"name"}
+          value={name}
+          onChangeText={(text) => setName(text)}
+        />
+        <Input
+          style={"inputLight"}
+          placeholder={"LastName"}
+          value={lastname}
+          onChangeText={(text) => setLastname(text)}
+        />
+        <Input
+          style={"inputLight"}
+          placeholder={"Email"}
+          value={email}
+          onChangeText={(text) => setEmail(text)}
+        />
+        <Input
+          style={"inputLight"}
+          placeholder={"Password"}
+          value={password}
+          onChangeText={(text) => setPassword(text)}
+        />
 
-      <Button
-        width={"60%"}
-        bgColor={Colors.accentColor}
-        color={"#fff"}
-        onPress={registerUser}
-        text={"REGISTER"}
-      />
-      {/* <Text onPress={props.showLogin} style={styles.registerText}>
-        Cancel
-      </Text> */}
-    </View>
+        <Button
+          width={"60%"}
+          bgColor={Colors.accentColor}
+          color={"#fff"}
+          onPress={registerUser}
+          text={"REGISTER"}
+        />
+        {/* <Text onPress={props.showLogin} style={styles.registerText}>
+          Cancel
+        </Text> */}
+      </View>
+    </Screen>
   );
 };
 
